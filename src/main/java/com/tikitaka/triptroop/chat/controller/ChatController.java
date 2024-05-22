a71535d3f97d045ecdd1906ff23efe9278437e2c
CHO HYONGKI <120306336+chk2023@users.noreply.github.com>
2024-05-22 13:08:03 +0900

    #7 채팅 패키지 작성 (#8)
    * chore: 채팅 패키지 작성
    
    * feat: 매핑 경로 지정

---
+++ src/main/java/com/tikitaka/triptroop/chat/controller/ChatController.java
@@ -0,0 +1,14 @@
+package com.tikitaka.triptroop.chat.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api") // <- "" 안에 매핑할 주소를 적어주세요. ( ex) /schedule/post )
+@RequiredArgsConstructor
+public class ChatController {
+
+    /* 내용을 작성해주세요. */
+
+}
---
+++ src/main/java/com/tikitaka/triptroop/chat/domain/repository/ChatRepository.java
@@ -0,0 +1,11 @@
+package com.tikitaka.triptroop.chat.domain.repository;
+
+import com.tikitaka.triptroop.chat.domain.entity.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/* Entity에는 Entity명 적기 */
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+
+    /* Repository 앞에 부분을 지우고 본인의 기능 폴더명을 적어주세요. */
+
+}
