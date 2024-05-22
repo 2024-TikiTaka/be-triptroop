c862d647c9416ab187ed08117b6cea9d6df7457f
may54ther <aha9654@gmail.com>
2024-05-22 17:00:16 +0900

    chore: import 문제로 entity 명 변경

--- src/main/java/com/tikitaka/triptroop/_example/domain/entity/Example.java
+++ src/main/java/com/tikitaka/triptroop/_example/domain/entity/Example.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@jakarta.persistence.Entity
+@Entity
 @Table(name = "") // <- ""안에 엔티티 매핑할 테이블 명 적어주세요.
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class Entity { // <- Entity 를 본인의 엔티티 명으로 바꿔 주세요. ( ! 첫문자 대문자임 (ex-Member O / member X) )
+public class Example { // <- Entity 를 본인의 엔티티 명으로 바꿔 주세요. ( ! 첫문자 대문자임 (ex-Member O / member X) )
 
     /* 매핑할 필드( 컬럼명 : 카멜케이스 )를 적어주세요. */
     @Id
--- src/main/java/com/tikitaka/triptroop/_example/domain/repository/ExampleRepository.java
+++ src/main/java/com/tikitaka/triptroop/_example/domain/repository/ExampleRepository.java
@@ -1,10 +1,10 @@
 package com.tikitaka.triptroop._example.domain.repository;
 
-import com.tikitaka.triptroop._example.domain.entity.Entity;
+import com.tikitaka.triptroop._example.domain.entity.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /* Entity에는 Entity명 적기 */
-public interface ExampleRepository extends JpaRepository<Entity, Long> {
+public interface ExampleRepository extends JpaRepository<Example, Long> {
 
     /* Repository 앞에 부분을 지우고 본인의 기능 폴더명을 적어주세요. */
 
