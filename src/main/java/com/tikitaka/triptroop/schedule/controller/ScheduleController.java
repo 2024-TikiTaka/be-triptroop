858f56abe4e61ed38b0c3a6fe254fe8d541fe4ec
EUNJAE97 <154950170+EUNJAE97@users.noreply.github.com>
2024-05-22 12:46:06 +0900

    chore : 일정 패키지 작성

---
+++ src/main/java/com/tikitaka/triptroop/schedule/controller/ScheduleController.java
@@ -0,0 +1,14 @@
+package com.tikitaka.triptroop.schedule.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("") // <- "" 안에 매핑할 주소를 적어주세요. ( ex) /schedule/post )
+@RequiredArgsConstructor
+public class ScheduleController {
+
+    /* 내용을 작성해주세요. */
+
+}
