099ed97f22261d80e06c80e93958ca167eed0865
EUNJAE97 <154950170+EUNJAE97@users.noreply.github.com>
2024-05-22 12:54:38 +0900

    1# 일정 패키지 작성 (#3)
    chore : 일정 패키지 작성

---
+++ src/main/java/com/tikitaka/triptroop/schedule/dto/response/ScheduleResponse.java
@@ -0,0 +1,13 @@
+package com.tikitaka.triptroop.schedule.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ScheduleResponse { //<- 무슨 응답인지 알수 있게 Response 앞에 명시해주세요. ( ex) 프로필 응답 일때 -> ProfileResponse )
+
+    /* 응답시 사용할 필드명을 입력해주세요. */
+
+
+}
