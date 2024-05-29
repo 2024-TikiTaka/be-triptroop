0f22127cb4ff92bfe2e40511f61a1a2a05862ff2
may54ther <aha9654@gmail.com>
2024-05-29 13:20:47 +0900

    feat: BaseTimeEntity 변경

--- src/main/java/com/tikitaka/triptroop/companions/domain/entity/Companion.java
+++ src/main/java/com/tikitaka/triptroop/companions/domain/entity/Companion.java
@@ -1,6 +1,6 @@
 package com.tikitaka.triptroop.companions.domain.entity;
 
-import com.tikitaka.triptroop.common.domain.entity.BaseTimeEntity;
+import com.tikitaka.triptroop.common.domain.BaseTimeEntity;
 import com.tikitaka.triptroop.companions.domain.type.OpenStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -14,19 +14,30 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Companion extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "companion_id")
     private Long id;
+
     private Long userId;
+
     private Long scheduleId;
+
     private String ageRestriction;
+
     private String genderRestriction;
+
     private String title;
+
     private String content;
+
     private Integer views;
+
     private OpenStatus status;
+
     private Boolean isDeleted = false;
+
     private LocalDateTime deletedAt;
 
 }
