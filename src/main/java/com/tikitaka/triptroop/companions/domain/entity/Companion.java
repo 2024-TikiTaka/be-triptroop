cec7ea82c3e56e9a5f6e25007b386ee0359ed4bd
EUNJAE97 <154950170+EUNJAE97@users.noreply.github.com>
2024-05-29 10:11:17 +0900

    chore: 동행글,사용자 후기 엔티티 작성

---
+++ src/main/java/com/tikitaka/triptroop/companions/domain/entity/Companion.java
@@ -0,0 +1,32 @@
+package com.tikitaka.triptroop.companions.domain.entity;
+
+import com.tikitaka.triptroop.common.domain.entity.BaseTimeEntity;
+import com.tikitaka.triptroop.companions.domain.type.OpenStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "companions")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Companion extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "companion_id")
+    private Long id;
+    private Long userId;
+    private Long scheduleId;
+    private String ageRestriction;
+    private String genderRestriction;
+    private String title;
+    private String content;
+    private Integer views;
+    private OpenStatus status;
+    private Boolean isDeleted = false;
+    private LocalDateTime deletedAt;
+
+}
---
+++ src/main/java/com/tikitaka/triptroop/userReview/domain/entity/UserReview.java
@@ -0,0 +1,26 @@
+package com.tikitaka.triptroop.userReview.domain.entity;
+
+import com.tikitaka.triptroop.common.domain.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_reviews")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserReview extends BaseTimeEntity {
+    @Id
+    @Column(name = "user_review_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long companionId;
+    private Long reviewerId;
+    private Long reviewedUserId;
+    private Integer reviewPoint;
+    private LocalDateTime deletedAt;
+
+}
