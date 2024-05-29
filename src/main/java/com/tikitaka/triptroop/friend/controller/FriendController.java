5a442fc210038eb7b3410c2caaa752dfc38c0e61
chk2023 <jidanmian777@gmail.com>
2024-05-29 10:12:54 +0900

    chore: 친구 도메인 패키지 작성

--- src/main/java/com/tikitaka/triptroop/friend/controller/FriendController.java
+++ src/main/java/com/tikitaka/triptroop/friend/controller/FriendController.java
@@ -1,18 +1,18 @@
-package com.tikitaka.triptroop.friend.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1/example") // <- "" 안에 매핑할 주소를 적어주세요. ( ex) /schedule/post )
-@RequiredArgsConstructor
-public class FriendController {
-
-    /* 내용을 작성해주세요. */
-    @RequestMapping("/hello")
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello");
-    }
-}
+//package com.tikitaka.triptroop.friend.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/v1/example") // <- "" 안에 매핑할 주소를 적어주세요. ( ex) /schedule/post )
+//@RequiredArgsConstructor
+//public class FriendController {
+//
+//    /* 내용을 작성해주세요. */
+//    @RequestMapping("/hello")
+//    public ResponseEntity<String> hello() {
+//        return ResponseEntity.ok("Hello");
+//    }
+//}
