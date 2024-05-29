23236d64d50ffe36635754ad8356c4b2d5790982
may54ther <aha9654@gmail.com>
2024-05-29 11:21:05 +0900

    feat: 회원 가입 기능 추가

--- src/main/java/com/tikitaka/triptroop/user/controller/SignUpController.java
+++ src/main/java/com/tikitaka/triptroop/user/controller/SignUpController.java
@@ -1,18 +1,30 @@
 package com.tikitaka.triptroop.user.controller;
 
+import com.tikitaka.triptroop.common.dto.response.ApiResponse;
+import com.tikitaka.triptroop.user.dto.request.SignUpRequest;
 import com.tikitaka.triptroop.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/signup")
 @RequiredArgsConstructor
 public class SignUpController {
 
     private final UserService userService;
 
+    @PostMapping()
+    public ResponseEntity<ApiResponse<Void>> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
+        userService.signup(signUpRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("회원가입이 완료되었습니다."));
+    }
 
 }
--- src/main/java/com/tikitaka/triptroop/user/service/UserService.java
+++ src/main/java/com/tikitaka/triptroop/user/service/UserService.java
@@ -1,11 +1,17 @@
 package com.tikitaka.triptroop.user.service;
 
+import com.tikitaka.triptroop.common.exception.ConflictException;
+import com.tikitaka.triptroop.common.exception.type.ExceptionCode;
+import com.tikitaka.triptroop.user.domain.entity.User;
 import com.tikitaka.triptroop.user.domain.repository.UserRepository;
+import com.tikitaka.triptroop.user.dto.request.SignUpRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -15,5 +21,28 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
+    public void signup(final SignUpRequest userRequest) {
 
+        existsByEmail(userRequest.getEmail());
+
+        final User newUser = User.of(
+                userRequest.getEmail(),
+                passwordEncoder.encode(userRequest.getPassword()),
+                userRequest.getName(),
+                userRequest.getBirth(),
+                userRequest.getGender()
+        );
+
+        userRepository.save(newUser);
+        log.info("✅signUp ::: newUser : {} ", newUser);
+    }
+
+    @Transactional(readOnly = true)
+    public void existsByEmail(String email) {
+        log.info("={}", userRepository.existsByEmail(email));
+        if (userRepository.existsByEmail(email)) {
+            throw new ConflictException(ExceptionCode.ALREADY_EXISTS_EMAIL);
+        }
+    }
 }
