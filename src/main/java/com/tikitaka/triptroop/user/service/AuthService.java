7b3027e878eb638d301a2385ccdafbfb5d6385d7
may54ther <aha9654@gmail.com>
2024-05-30 16:56:44 +0900

    feat: 로그인 및 토큰 발급 기능

--- src/main/java/com/tikitaka/triptroop/user/service/AuthService.java
+++ src/main/java/com/tikitaka/triptroop/user/service/AuthService.java
@@ -2,26 +2,82 @@ package com.tikitaka.triptroop.user.service;
 
 
 import com.tikitaka.triptroop.common.security.dto.LoginDto;
+import com.tikitaka.triptroop.common.security.dto.TokenDto;
+import com.tikitaka.triptroop.common.security.util.TokenUtils;
+import com.tikitaka.triptroop.user.domain.type.CustomUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class AuthService implements UserDetailsService {
 
+    private final UserService userService;
+
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        LoginDto loginDto = new LoginDto();
+        LoginDto loginDto = userService.findByEmail(username);
 
-        return User.builder()
-                   .username(loginDto.getEmail())
-                   .password(loginDto.getPassword())
-                   .roles(loginDto.getRole().name())
-                   .build();
+        return org.springframework.security.core.userdetails.User.builder()
+                                                                 .username(loginDto.getEmail())
+                                                                 .password(loginDto.getPassword())
+                                                                 .roles(loginDto.getRole().name())
+                                                                 .build();
+    }
+
+    /**
+     * RefreshToken 수정
+     */
+    public void updateRefreshToken(String email, String refreshToken) {
+        userService.updateRefreshToken(email, refreshToken);
+    }
+
+    /**
+     * RefreshToken 검증 및 재발급
+     */
+    public TokenDto checkRefreshTokenAndReIssueToken(String refreshToken) {
+        LoginDto loginDto = userService.findByRefreshToken(refreshToken);
+        String reIssuedRefreshToken = TokenUtils.createRefreshToken();
+        String reIssuedAccessToken = TokenUtils.createAccessToken(getUserInfo(loginDto));
+        userService.updateRefreshToken(loginDto.getEmail(), reIssuedRefreshToken);
+        return TokenDto.of(reIssuedAccessToken, reIssuedRefreshToken);
+    }
+
+    /**
+     * Authentication 저장
+     */
+    public void saveAuthentication(String email) {
+
+        LoginDto loginDto = userService.findByEmail(email);
+
+        UserDetails user
+                = org.springframework.security.core.userdetails.User.builder()
+                                                                    .username(loginDto.getEmail())
+                                                                    .password(loginDto.getPassword())
+                                                                    .roles(loginDto.getRole().name())
+                                                                    .build();
+
+        CustomUser customUser = new CustomUser(loginDto.getUserId(), user);
+
+        Authentication authentication
+                = new UsernamePasswordAuthenticationToken(customUser, null, customUser.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private Map<String, Object> getUserInfo(LoginDto loginDto) {
+        return Map.of(
+                "username", loginDto.getEmail(),
+                "role", loginDto.getRole()
+        );
     }
 }
