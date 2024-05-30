ccc77dcb9c3b1dd1bb99ee73c060af5bfddc16be
may54ther <aha9654@gmail.com>
2024-05-30 16:55:46 +0900

    refactor: Security 설정 수정

--- src/main/java/com/tikitaka/triptroop/common/security/handler/JwtAccessDeniedHandler.java
+++ src/main/java/com/tikitaka/triptroop/common/security/handler/JwtAccessDeniedHandler.java
@@ -1,25 +1,29 @@
 package com.tikitaka.triptroop.common.security.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tikitaka.triptroop.common.dto.response.ApiResponse;
+import com.tikitaka.triptroop.common.exception.dto.response.ErrorResponse;
+import com.tikitaka.triptroop.common.exception.type.ExceptionCode;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import java.io.IOException;
 
-@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
-    private final ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        /* 리소스에 접근 권한이 없는데 접근하여 인가 되지 않은 경우 => 403 오류 */
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setContentType("application/json;charset=UTF-8");
-        // response.getWriter().write(objectMapper.writeValueAsString(new ExceptionResponse(ACCESS_DENIED)));
+        response.getWriter().write(objectMapper.writeValueAsString(
+                ApiResponse.fail(new ErrorResponse(ExceptionCode.ACCESS_DENIED)))
+        );
     }
 }
--- src/main/java/com/tikitaka/triptroop/common/security/handler/LoginFailureHandler.java
+++ src/main/java/com/tikitaka/triptroop/common/security/handler/LoginFailureHandler.java
@@ -1,10 +1,14 @@
 package com.tikitaka.triptroop.common.security.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tikitaka.triptroop.common.dto.response.ApiResponse;
+import com.tikitaka.triptroop.common.exception.dto.response.ErrorResponse;
+import com.tikitaka.triptroop.common.exception.type.ExceptionCode;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
@@ -13,12 +17,15 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
-    private final ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json;charset=UTF-8");
-        // response.getWriter().write(objectMapper.writeValueAsString(new ErrorResponse()));
+        response.getWriter().write(objectMapper.writeValueAsString(
+                ApiResponse.fail(new ErrorResponse(ExceptionCode.UNAUTHORIZED)))
+        );
     }
 }
