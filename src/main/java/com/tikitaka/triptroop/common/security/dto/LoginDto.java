4ab3e70c6bd38e9671c9d71ec6ddf79819c69d46
may54ther <aha9654@gmail.com>
2024-05-30 16:54:06 +0900

    refactor: LoginDto에 from 메서드 추가

--- src/main/java/com/tikitaka/triptroop/common/security/dto/LoginDto.java
+++ src/main/java/com/tikitaka/triptroop/common/security/dto/LoginDto.java
@@ -1,5 +1,6 @@
 package com.tikitaka.triptroop.common.security.dto;
 
+import com.tikitaka.triptroop.user.domain.entity.User;
 import com.tikitaka.triptroop.user.domain.type.UserRole;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -16,4 +17,13 @@ public class LoginDto {
     private final String password;
 
     private final UserRole role;
+
+    public static LoginDto from(User user) {
+        return new LoginDto(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getRole()
+        );
+    }
 }
