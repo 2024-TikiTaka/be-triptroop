e1e3c7388fe52df3dae3aa23dd6d384f33595dfc
ddalla0425 <154950177+ddalla0425@users.noreply.github.com>
2024-05-30 21:52:01 +0900

    #67 신고 목록 네이밍 변경, 동행글 타입 추가 및 수정 (#68)
    * chore: 패키지 정리
    
    * 임시저장
    
    * 임시저장
    
    * refactor: @Column 어노테이션 수정
    
    * feat: 나의 신고 목록 조회 OK / 검색 및 필터링 : X 진행예정
    
    * refactor: companion enum type 추가
    
    * feat: 신고 조회 기능 네이밍 수정
    
    ---------
    
    Co-authored-by: qkrquddjs1 <qkrquddjs9752@naver.com>
    Co-authored-by: may54ther <aha9654@gmail.com>

---
+++ src/main/java/com/tikitaka/triptroop/companion/domain/type/AgeRestriction.java
@@ -0,0 +1,11 @@
+package com.tikitaka.triptroop.companion.domain.type;
+
+public enum AgeRestriction {
+    // 동행글 나이
+    TEENS,    //10대
+    TWENTIES, //20대
+    THIRTIES, //30대
+    FORTIES,  //40대
+    FIFTIES,  //50대
+    SENIORS   //시니어 (60이후)
+}
