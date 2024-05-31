2b7879670c40c21ce5e6784b9fb140c9ff6a585e
may54ther <aha9654@gmail.com>
2024-05-31 15:07:42 +0900

    feat: 로그아웃 완료

--- src/main/java/com/tikitaka/triptroop/user/AuthController/AuthController.java
+++ src/main/java/com/tikitaka/triptroop/user/AuthController/AuthController.java
@@ -1,4 +1,4 @@
-package com.tikitaka.triptroop.user.controller;
+package com.tikitaka.triptroop.user.AuthController;
 
 import com.tikitaka.triptroop.common.dto.response.ApiResponse;
 import com.tikitaka.triptroop.user.dto.request.SignUpRequest;
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/signup")
+@RequestMapping("/api/v1/")
 @RequiredArgsConstructor
-public class SignUpController {
+public class AuthController {
 
     private final UserService userService;
 
-    @PostMapping()
+    @PostMapping("/signup")
     public ResponseEntity<ApiResponse<Void>> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
         userService.signup(signUpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("회원가입이 완료되었습니다."));
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@AuthenticationPrincipal UserDetails userDetails) {
+
+        userService.updateRefreshToken(userDetails.getUsername(), null);
+        return ResponseEntity.ok().build();
+    }
 }
