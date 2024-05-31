e5b4eec27595e8b91dafe822adaa344da73fd920
아현 Ahyeon <aha9654@gmail.com>
2024-05-31 18:17:56 +0900

    #69, #70 로그아웃, 유저 정보 및 프로필 조회 (#73)
    * chore: .gitmodules 에 기본 브랜치 수정
    
    * chore: submodule 브랜치를 config/main 으로 수정
    
    * feat: 로그인 실패 예외 변경
    
    * feat: 로그아웃 완료
    
    * test: 회원가입 테스트
    
    * feat: 회원 아이디로 프로필 조회
    
    * test: 회원 정보, 프로필 정보 조회 성공 테스트 코드
    
    * chore: 주석 추가

--- src/main/java/com/tikitaka/triptroop/user/dto/response/UserDetailResponse.java
+++ src/main/java/com/tikitaka/triptroop/user/dto/response/UserDetailResponse.java
@@ -1,14 +1,18 @@
-package com.tikitaka.triptroop.user.dto.request;
+package com.tikitaka.triptroop.user.dto.response;
 
 import com.tikitaka.triptroop.user.domain.entity.User;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+/**
+ * 회원 정보 조회
+ */
 @Getter
 @RequiredArgsConstructor
-public class UserRequest {
+public class UserDetailResponse {
 
     private final Long id;
 
@@ -16,11 +20,18 @@ public class UserRequest {
 
     private final String name;
 
+    private LocalDate birth;
+
+    private String gender;
+
+    private String phone;
+
+    private Integer godo = 38;
+
     private final LocalDateTime createdAt;
 
-
-    public static UserRequest from(User user) {
-        return new UserRequest(
+    public static UserDetailResponse of(User user) {
+        return new UserDetailResponse(
                 user.getId(),
                 user.getEmail(),
                 user.getName(),
