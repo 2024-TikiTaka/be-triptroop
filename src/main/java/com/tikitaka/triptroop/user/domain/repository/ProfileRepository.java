e01999d595d23f73351d6d4848c0a68359efd041
may54ther <aha9654@gmail.com>
2024-06-04 11:06:52 +0900

    refactor: JPQL로 수정

--- src/main/java/com/tikitaka/triptroop/user/domain/repository/ProfileRepository.java
+++ src/main/java/com/tikitaka/triptroop/user/domain/repository/ProfileRepository.java
@@ -1,17 +1,37 @@
 package com.tikitaka.triptroop.user.domain.repository;
 
 import com.tikitaka.triptroop.user.domain.entity.Profile;
+import com.tikitaka.triptroop.user.dto.response.UserProfileResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
-    Optional<Profile> findByUserId(Long userId);
+    Boolean existsProfileByUserId(Long userId);
+
+    Boolean existsByNickname(String nickname);
+
+    Optional<Profile> findProfileByUserId(Long userId);
 
     /* 다솔 - 신고 목록 닉네임 조회 테스트 */
-    Profile findByNickname(String nickname);
+    Optional<Profile> findByNickname(String nickname);
 
-    List<Profile> findByUserIdIn(List<Long> userIds);
+    @Query("SELECT new com.tikitaka.triptroop.user.dto.response.UserProfileResponse(u, p)" +
+            "FROM Profile p LEFT JOIN User u ON u.id = p.userId " +
+            "WHERE u.status = 'ACTIVE' AND p.userId = :userId")
+    Optional<UserProfileResponse> findUserProfileByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT new com.tikitaka.triptroop.user.dto.response.UserProfileResponse(u, p)" +
+            "FROM Profile p LEFT JOIN User u ON u.id = p.userId " +
+            "WHERE u.status = 'ACTIVE' AND p.nickname = :nickname")
+    Optional<UserProfileResponse> findUserProfileByNickname(@Param("nickname") String nickname);
+
+    @Query("SELECT new com.tikitaka.triptroop.user.dto.response.UserProfileResponse(u, p)" +
+            "FROM Profile p LEFT JOIN User u ON u.id = p.userId " +
+            "WHERE u.status = 'ACTIVE' AND u.id IN :userIds")
+    List<UserProfileResponse> findUserProfileListByUserId(@Param("userIds") List<Long> userIds);
 }
