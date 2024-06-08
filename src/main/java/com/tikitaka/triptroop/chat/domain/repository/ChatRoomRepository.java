f70099024ce16e2dd0641f926e2c5a462c0ae83b
CHO HYONGKI <jidanmian777@gmail.com>
2024-06-08 17:31:54 +0900

    feat: 개인 채팅방 조회 추가

--- src/main/java/com/tikitaka/triptroop/chat/domain/repository/ChatRoomRepository.java
+++ src/main/java/com/tikitaka/triptroop/chat/domain/repository/ChatRoomRepository.java
@@ -4,8 +4,10 @@ import com.tikitaka.triptroop.chat.domain.entity.ChatRoom;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ChatRoomRepository extends MongoRepository<ChatRoom, ObjectId> {
+    List<ChatRoom> findByMemberContaining(Long username);
 
 
-//    ChatRoom createChatRoom(@Valid ChatRequest request);
 }
