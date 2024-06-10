5dbf899d3005f9337bb0eee08745cc03249faa6e
CHO HYONGKI <120306336+chk2023@users.noreply.github.com>
2024-06-10 17:09:31 +0900

    chore: 알림 패키지 작성 (#173)

---
+++ src/main/java/com/tikitaka/triptroop/notification/domain/type/NotificationType.java
@@ -0,0 +1,13 @@
+package com.tikitaka.triptroop.notification.domain.type;
+
+public enum NotificationType {
+
+    LIKE,  //좋아요
+    FRIEND_REQUESTED, //친구 신청
+    SCHEDULE_ACCEPTED, //일정 수락
+    SCHEDULE_REJECTED, //일정 거절
+    REPORT_COMPLETED, //신고 완료
+    QNA //Q&A
+
+
+}
---
+++ src/main/java/com/tikitaka/triptroop/notification/dto/request/NotificationRequest.java
@@ -0,0 +1,10 @@
+package com.tikitaka.triptroop.notification.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class NotificationRequest {
+}
