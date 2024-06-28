784b99b1242e5aa24e245c28bfab483ef03f2731
may54ther <aha9654@gmail.com>
2024-06-28 22:01:30 +0900

    refactor: OAuth2User, UserDetails 구현체로 변경

--- src/main/java/com/tikitaka/triptroop/user/domain/type/CustomUser.java
+++ src/main/java/com/tikitaka/triptroop/user/domain/type/CustomUser.java
@@ -1,24 +1,84 @@
 package com.tikitaka.triptroop.user.domain.type;
 
-import lombok.Getter;
+import com.tikitaka.triptroop.user.domain.entity.User;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
+
+import lombok.Getter;
 
 @Getter
-public class CustomUser extends User {
+public class CustomUser implements OAuth2User, UserDetails {
 
     private Long userId;
 
-    public CustomUser(Long userId, String username, String password, UserRole role) {
-        super(username, password, Collections.singletonList(new SimpleGrantedAuthority(role.name())));
+    private String email;
+
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    private Map<String, Object> attributes;
+
+    public CustomUser(Long userId, String email, String password, UserRole role) {
         this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.authorities = Collections.singletonList(new SimpleGrantedAuthority(role.name()));
     }
 
-    public CustomUser(Long userId, UserDetails userDetails) {
-        super(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
-        this.userId = userId;
+    public CustomUser(User user, Map<String, Object> attributes) {
+        this(user.getId(), user.getEmail(), user.getPassword(), user.getRole());
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return String.valueOf(userId);
     }
 }
